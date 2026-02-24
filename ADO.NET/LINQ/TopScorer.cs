90c0265636dcb6cc7b477173f529d4d666893297
Ramangoyal565 <ramangoyal565@gmail.com>
2026-02-24 15:08:23 +0530

    Linq on CSV

---
+++ ADO.NET/LINQ/TopScorer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    public class TopScorer
+    {
+        public static void main()
+        {
+            List<string> names = new List<string>() 
+            { "Ravi,87",
+            "Kumar,98",
+            "Arun,92",
+            "Deepak,80",
+            "Kiran,95"};
+
+            List<string> topScorers = names.Select(s => s.Split(","))
+            .Select(p => new { Name = p[0], Marks = int.Parse(p[1]) })
+            .OrderByDescending(s=>s.Marks).Take(3)
+            .Select(s=>s.Name).ToList();
+
+            foreach (var name in topScorers)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
