eebd60b7567f0b6e0a9e80b81bab438b5cec24cf
Ramangoyal565 <ramangoyal565@gmail.com>
2026-02-24 15:35:04 +0530

    ADO net

--- ADO.NET/ADO.NET_ReadData/Program.cs
+++ ADO.NET/ADO.NET_ReadData/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+class Program
+{
+    public static void Main()
+    {
+        string connectionString =
+                "Server=localhost\\SQLEXPRESS;" +
+                "Database=TopBrains;" +
+                "Trusted_Connection=True;" +
+                "TrustServerCertificate=True;";
+        try
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Select * from Product";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    using SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["Name"]}, Price: {reader["Price"]}");
+                    }
+                }
+                con.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
