3d4cb1da0e4f0a8281b0460679778acc29f15048
Ramangoyal565 <ramangoyal565@gmail.com>
2026-03-06 14:41:45 +0530

    MVC Assignment

---
+++ MVC/TempData/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TempData.Models;
+namespace TempData.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] ="Student created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+    }
+}
