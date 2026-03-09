58251bfa672149a400f35cea7370d6e4a30a8eda
Ramangoyal565 <ramangoyal565@gmail.com>
2026-03-09 14:37:06 +0530

    Image Upload

---
+++ MVC/FileUpload/Controllers/ImageController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace FileUpload.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                string extension = Path.GetExtension(file.FileName).ToLower();
+
+                if ((extension == ".jpg" || extension == ".png") && file.Length <= 2 * 1024 * 1024)
+                {
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Uploads");
+
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    string filePath = Path.Combine(path, file.FileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    ViewBag.Message = "Uploaded";
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid File";
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Invalid File";
+            }
+
+            return View("Index");
+        }
+    }
+}
